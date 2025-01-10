3f2d7c37170a387cfebc3e6afe3f75ad2edaae6e
星光-k <36470587+kxgx@users.noreply.github.com>
2025-01-10 07:07:02 +0000

    更新 start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+
 f_name=main.py
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 logdir=${dir%/*}
-pid=`ps -ef |grep $dir/$f_name | grep -v grep |awk '{print $2}'`
-for id in $pid
-do
-    kill -9 $id
+screen_name="main_screen"
+
+# 查找并结束所有与指定screen_name相关的screen会话
+pids=$(screen -ls | grep $screen_name | awk -F '.' '{print $1}' | awk '{print $1}')
+for pid in $pids; do
+    screen -S $pid -X quit
 done
-nohup /usr/bin/python3 -u $dir/$f_name > $logdir/log/info.log 2>&1 &
+
+# 创建一个新的screen会话并运行Python脚本
+screen -dmS $screen_name /usr/bin/python3 -u $dir/$f_name > $logdir/log/info.log 2>&1
