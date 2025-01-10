c007270ae7d1278ff601d8ec0d8fc38540476123
星光-k <36470587+kxgx@users.noreply.github.com>
2025-01-10 22:24:19 +0800

    Create stop.sh

---
+++ bin/stop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+f_name=clean.py
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+logdir=${dir%/*}
+screen_name="clean_screen"
+
+# 查找并结束所有与指定screen_name相关的screen会话
+pids=$(screen -ls | grep $screen_name | awk -F '.' '{print $1}' | awk '{print $1}')
+for pid in $pids; do
+    screen -S $pid -X quit
+done
+
+# 创建一个新的screen会话并运行Python脚本
+screen -dmS $screen_name /usr/bin/python3 -u $dir/$f_name > $logdir/log/info.log 2>&1
