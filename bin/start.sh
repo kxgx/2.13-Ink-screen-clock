0565fd6cf5872faa7bbc19d54d2c73f5dd76d4d6
星光-k <36470587+kxgx@users.noreply.github.com>
2025-01-12 16:18:33 +0800

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -2,14 +2,17 @@
 
 f_name=main.py
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-logdir=${dir%/*}
+logdir=${dir%/*}/log
 screen_name="main_screen"
 
-# 查找并结束所有与指定screen_name相关的screen会话
+# 创建日志目录，如果它不存在的话
+mkdir -p $logdir
+
+# 结束与指定screen_name相关的所有screen会话（可选，如果您想要保留这个步骤）
 pids=$(screen -ls | grep $screen_name | awk -F '.' '{print $1}' | awk '{print $1}')
 for pid in $pids; do
     screen -S $pid -X quit
 done
 
-# 创建一个新的screen会话并运行Python脚本
-screen -dmS $screen_name /usr/bin/python3 -u $dir/$f_name > $logdir/log/info.log 2>&1
+# 使用nohup运行Python脚本，并将输出重定向到日志文件
+nohup /usr/bin/python3 -u $dir/$f_name > $logdir/info.log 2>&1 &
