0db4d98671dd162f2009fb44eb4014f19c6d439e
星光-k <36470587+kxgx@users.noreply.github.com>
2025-01-13 15:48:42 +0800

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -192,7 +192,7 @@ install_packages() {
     exit 1
   fi
     echo "正在安装软件包"
-  if ! sudo apt-get install -y git pigpio i2c-tools netcat* gawk python3-dev python3-pip python3-pil python3-numpy python3-gpiozero python3-pigpio build-essential; then
+  if ! sudo apt-get install -y -q git pigpio i2c-tools netcat* gawk python3-dev python3-pip python3-pil python3-numpy python3-gpiozero python3-pigpio build-essential; then
     echo "软件包安装失败" >&2
     exit 1
   fi
