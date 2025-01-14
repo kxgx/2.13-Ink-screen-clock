531f5a443b550ec4a6bb3b7428c45596bbcb81f0
星光-k <36470587+kxgx@users.noreply.github.com>
2025-01-14 19:16:16 +0800

    Update clean.sh

--- bin/clean.sh
+++ bin/clean.sh
@@ -8,7 +8,7 @@ service_name="raspi_e-Paper.service"
 hold_time_seconds=10  # 设置维持时间，例如300秒（5分钟）
 
 # 创建一个新的screen会话并运行Python脚本
-screen -dmS $screen_name /usr/bin/python3 -u $dir/$f_name 2>&1
+screen -dmS $screen_name /usr/bin/python3 -u $dir/$f_name
 
 # 等待维持时间
 echo "正在运行脚本，将在 ${hold_time_seconds} 秒后停止服务..."
