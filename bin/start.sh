95e6bb12be49ed0c48c22605696826975b084bcd
星光-k <36470587+kxgx@users.noreply.github.com>
2025-01-16 11:54:01 +0000

    Add files via upload

--- bin/start.sh
+++ bin/start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 f_name=main.py
 f1_name=weather.py
+f2_name=app.py
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 logdir="${dir%/*}/log"
+appdir="${dir%/*}/app"
 # 确保日志目录存在
 mkdir -p "${logdir}"
 pid=`ps -ef |grep $dir/$f_name | grep -v grep |awk '{print $2}'`
@@ -12,3 +14,4 @@ do
 done
 nohup /usr/bin/python3 -u $dir/$f_name > $logdir/info.log 2>&1 &
 nohup /usr/bin/python3 -u $dir/$f1_name > $logdir/info-wenter.log 2>&1 &
+nohup /usr/bin/python3 -u $appdir/$f2_name > $logdir/info-app.log 2>&1 &
